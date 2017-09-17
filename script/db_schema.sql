1f16d99fd6bc3ad408175f368d174681e5f2f215
Mario Catuogno <catuogno.mario88@gmail.com>
2017-09-17 19:58:07 +0200

    Update db_schema.sql

--- script/db_schema.sql
+++ script/db_schema.sql
@@ -1,8 +1,35 @@
-SQLite Query
+--------------------------------------------------------------------------------
+-- DOMAIN TABLES
+--------------------------------------------------------------------------------
+-- CREATE TABLE `my_movie_genres`
+CREATE TABLE `my_movie_genres` (
+`genres_id` INTEGER AUTOINCREMENT UNIQUE,
+`genres_name` VARCHAR(50) NOT NULL PRIMARY KEY UNIQUE
+);
 
+-- CREATE TABLE `my_movie_media`
+CREATE TABLE `my_movie_media` (
+`media_id` INTEGER AUTOINCREMENT UNIQUE,
+`media_name` VARCHAR(50) NOT NULL PRIMARY KEY UNIQUE
+);
+
+-- CREATE TABLE `my_movie_language`
+CREATE TABLE `my_movie_language` (
+`language_id` INTEGER AUTOINCREMENT UNIQUE,
+`language_name` VARCHAR(50) NOT NULL PRIMARY KEY UNIQUE
+);
+
+-- CREATE TABLE `my_movie_country`
+CREATE TABLE `my_movie_country` (
+`country_id` INTEGER AUTOINCREMENT UNIQUE,
+`country_name` VARCHAR(50) NOT NULL PRIMARY KEY UNIQUE
+);
+--------------------------------------------------------------------------------
+-- MOVIES TABLES
+--------------------------------------------------------------------------------
 -- CREATE TABLE `my_movie_list`
 CREATE TABLE `my_movies_list` (
-`movie_id`	INTEGER AUTOINCREMENT UNIQUE,
+`movie_id` INTEGER AUTOINCREMENT UNIQUE,
 `imdb_id`	VARCHAR(10) NOT NULL PRIMARY KEY UNIQUE,
 `movie_title`	VARCHAR(255) NOT NULL,
 `movie_genre`	VARCHAR(50) NOT NULL,
@@ -22,6 +49,8 @@ CREATE TABLE `my_movies_list` (
 `movie_star1` VARCHAR(255),
 `movie_star2` VARCHAR(255),
 `movie_star3` VARCHAR(255),
+FOREIGN KEY(movie_genre) REFERENCES my_movie_genres(genres_name),
+FOREIGN KEY(release_country) REFERENCES my_movie_country(country_name)
 );
 
 -- CREATE TABLE `my_movie_seen`
@@ -35,4 +64,7 @@ CREATE TABLE `my_movies_seen` (
 `movie_language` VARCHAR(50),
 `movie_premiere` CHAR(1),
 `ticket_price` DOUBLE(2,1),
+FOREIGN KEY(media_type) REFERENCES my_movie_media(media_name),
+FOREIGN KEY(movie_language) REFERENCES my_movie_language(language_name)
 );
+--------------------------------------------------------------------------------
