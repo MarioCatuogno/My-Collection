466d9a73863bfead161cb74d7bb012acdc7a2ef8
Mario Catuogno <catuogno.mario88@gmail.com>
2017-09-18 23:15:27 +0200

    Update db_config_test.sql

--- db/db_config_test.sql
+++ db/db_config_test.sql
@@ -8,16 +8,6 @@ PRAGMA encoding = "UTF-8";
 --------------------------------------------------------------------------------
 -- TRIGGERS
 --------------------------------------------------------------------------------
--- CREATE TRIGGER `fk_lk_genres`
-CREATE TRIGGER fk_lk_genres
-BEFORE INSERT ON my_movies_list
-FOR EACH ROW BEGIN
-SELECT RAISE(ROLLBACK, 'Violated FK on movie_genre')
-WHERE  NEW.movie_genre IS NOT NULL AND
- (SELECT genres_name FROM lk_genres
-  WHERE genres_name = NEW.movie_genre) IS NULL;
-END;
-
 -- CREATE TRIGGER `fk_lk_country`
 CREATE TRIGGER fk_lk_country
 BEFORE INSERT ON my_movies_list
@@ -28,14 +18,24 @@ WHERE  NEW.release_country IS NOT NULL AND
   WHERE country_name = NEW.release_country) IS NULL;
 END;
 
--- CREATE TRIGGER `fk_lk_media`
-CREATE TRIGGER fk_lk_media
+-- CREATE TRIGGER `fk_lk_flag`
+CREATE TRIGGER fk_lk_flag
 BEFORE INSERT ON my_movies_seen
 FOR EACH ROW BEGIN
-SELECT RAISE(ROLLBACK, 'Violated FK on media_type')
-WHERE  NEW.media_type IS NOT NULL AND
- (SELECT media_name FROM lk_media
-  WHERE media_name = NEW.media_type) IS NULL;
+SELECT RAISE(ROLLBACK, 'Violated FK on movie_premiere')
+WHERE  NEW.movie_premiere IS NOT NULL AND
+ (SELECT flag_name FROM lk_flag
+  WHERE flag_name = NEW.movie_premiere) IS NULL;
+END;
+
+-- CREATE TRIGGER `fk_lk_genres`
+CREATE TRIGGER fk_lk_genres
+BEFORE INSERT ON my_movies_list
+FOR EACH ROW BEGIN
+SELECT RAISE(ROLLBACK, 'Violated FK on movie_genre')
+WHERE  NEW.movie_genre IS NOT NULL AND
+ (SELECT genres_name FROM lk_genres
+  WHERE genres_name = NEW.movie_genre) IS NULL;
 END;
 
 -- CREATE TRIGGER `fk_lk_language`
@@ -48,13 +48,13 @@ WHERE  NEW.movie_language IS NOT NULL AND
   WHERE language_name = NEW.movie_language) IS NULL;
 END;
 
--- CREATE TRIGGER `fk_lk_flag`
-CREATE TRIGGER fk_lk_flag
+-- CREATE TRIGGER `fk_lk_media`
+CREATE TRIGGER fk_lk_media
 BEFORE INSERT ON my_movies_seen
 FOR EACH ROW BEGIN
-SELECT RAISE(ROLLBACK, 'Violated FK on movie_premiere')
-WHERE  NEW.movie_premiere IS NOT NULL AND
- (SELECT language_name FROM lk_flag
-  WHERE language_name = NEW.movie_premiere) IS NULL;
+SELECT RAISE(ROLLBACK, 'Violated FK on media_type')
+WHERE  NEW.media_type IS NOT NULL AND
+ (SELECT media_name FROM lk_media
+  WHERE media_name = NEW.media_type) IS NULL;
 END;
 --------------------------------------------------------------------------------
