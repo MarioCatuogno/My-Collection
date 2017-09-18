a18b121768e6108f4f99cdb5659cd596aefcc375
Mario Catuogno <catuogno.mario88@gmail.com>
2017-09-18 22:55:40 +0200

    Update db_config_test.sql

--- db/db_config_test.sql
+++ db/db_config_test.sql
@@ -12,7 +12,7 @@ PRAGMA encoding = "UTF-8";
 CREATE TRIGGER fk_my_movies_genres
 BEFORE INSERT ON my_movies_list
 FOR EACH ROW BEGIN
-SELECT RAISE(ROLLBACK, 'Insert on table "my_movies_list" violates FK')
+SELECT RAISE(ROLLBACK, 'Violated FK on movie_genre')
 WHERE  NEW.movie_genre IS NOT NULL AND
  (SELECT genres_name FROM my_movies_genres
   WHERE genres_name = NEW.movie_genre) IS NULL;
@@ -22,8 +22,19 @@ END;
 CREATE TRIGGER fk_my_movies_media
 BEFORE INSERT ON my_movies_seen
 FOR EACH ROW BEGIN
-SELECT RAISE(ROLLBACK, 'Insert on table "my_movies_seen" violates FK')
+SELECT RAISE(ROLLBACK, 'Violated FK on media_type')
 WHERE  NEW.media_type IS NOT NULL AND
  (SELECT media_name FROM my_movies_media
   WHERE media_name = NEW.media_type) IS NULL;
 END;
+
+-- CREATE TRIGGER `fk_my_movies_language`
+CREATE TRIGGER fk_my_movies_language
+BEFORE INSERT ON my_movies_seen
+FOR EACH ROW BEGIN
+SELECT RAISE(ROLLBACK, 'Violated FK on movie_language')
+WHERE  NEW.movie_language IS NOT NULL AND
+ (SELECT language_name FROM my_movies_media
+  WHERE language_name = NEW.movie_language) IS NULL;
+END;
+--------------------------------------------------------------------------------
