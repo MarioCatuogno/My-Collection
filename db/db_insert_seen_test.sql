009f99bea273200c005cc0721401553a73c5984a
Mario Catuogno <catuogno.mario88@gmail.com>
2017-09-19 21:23:43 +0200

    Create db_insert_seen_test.sql

---
+++ db/db_insert_seen_test.sql
@@ -0,0 +1,23 @@
+--------------------------------------------------------------------------------
+-- INSERT DATA INTO SEEN TABLE
+--------------------------------------------------------------------------------
+-- TEMPLATE: INSERT INTO TABLE `my_movies_seen`
+INSERT INTO my_movies_list VALUES (
+ --movie_id,movie_date,movie_location
+ UPPER(''),UPPER(''),UPPER(''),
+ --media_type,media_detail,media_technology
+ UPPER(''),UPPER(''),UPPER(''),
+ --movie_language,movie_premiere,ticket_price
+ UPPER(''),UPPER(''),
+);
+--------------------------------------------------------------------------------
+-- CREATE TABLE `my_movies_seen`
+INSERT INTO my_movies_list VALUES (
+ --movie_id,movie_date,movie_location
+ UPPER('tt419287'),UPPER('2017-09-19'),UPPER('parigi'),
+ --media_type,media_detail,media_technology
+ UPPER('bd'),UPPER('blu-ray'),UPPER('4k'),
+ --movie_language,movie_premiere,ticket_price
+ UPPER('italian'),UPPER('y'),6.5
+);
+--------------------------------------------------------------------------------
