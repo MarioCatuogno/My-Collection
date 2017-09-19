2815b9aac2838e455a7580ca171cb6ed8b23e9a3
Mario Catuogno <catuogno.mario88@gmail.com>
2017-09-19 21:39:17 +0200

    Update db_insert_seen_test.sql

--- db/db_insert_seen_test.sql
+++ db/db_insert_seen_test.sql
@@ -1,23 +1,24 @@
 --------------------------------------------------------------------------------
--- INSERT DATA INTO SEEN TABLE
+-- TEMPLATE: INSERT DATA INTO SEEN TABLE
 --------------------------------------------------------------------------------
--- TEMPLATE: INSERT INTO TABLE `my_movies_seen`
 INSERT INTO my_movies_list VALUES (
- --movie_id,movie_date,movie_location
- UPPER(''),UPPER(''),UPPER(''),
- --media_type,media_detail,media_technology
- UPPER(''),UPPER(''),UPPER(''),
- --movie_language,movie_premiere,ticket_price
- UPPER(''),UPPER(''),
-);
---------------------------------------------------------------------------------
--- CREATE TABLE `my_movies_seen`
-INSERT INTO my_movies_list VALUES (
- --movie_id,movie_date,movie_location
- UPPER('tt419287'),UPPER('2017-09-19'),UPPER('parigi'),
- --media_type,media_detail,media_technology
- UPPER('bd'),UPPER('blu-ray'),UPPER('4k'),
- --movie_language,movie_premiere,ticket_price
- UPPER('italian'),UPPER('y'),6.5
+--movie_id
+UPPER('')
+--movie_date
+,UPPER('')
+--movie_location
+,UPPER('')
+--media_type
+,UPPER('')
+--media_detail
+,UPPER('')
+--media_technology
+,UPPER('')
+--movie_language
+,UPPER('')
+--movie_premiere
+,UPPER('')
+--ticket_price
+,
 );
 --------------------------------------------------------------------------------
