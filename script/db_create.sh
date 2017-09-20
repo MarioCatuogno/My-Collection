0dd73877cec801429acf26e2aa959fdd472ee368
Mario Catuogno <catuogno.mario88@gmail.com>
2017-09-20 22:33:43 +0200

    Update db_create.sh

--- script/db_create.sh
+++ script/db_create.sh
@@ -7,8 +7,10 @@ DBNAME=my_collection.db
 #Test if database exists, otherwise create the database
 cd $DBPATH
 if [ -f $DBNAME ]; then
-  echo "The database '$DBNAME' exists."
+ echo "The database '$DBNAME' exists."
+ ls -ltr $DBNAME
 else
-  echo "Creating database into: '$DBPATH'."
-sqlite3> $DBNAME .databases .exit
+ echo "Creating database into: '$DBPATH'."
+ sqlite3> $DBNAME .databases .exit
+ chmod ug+rwx $DBNAME
 fi
