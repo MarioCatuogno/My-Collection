1b8c2a97ace4115f827933cf08086df02532f5ff
Mario Catuogno <catuogno.mario88@gmail.com>
2017-10-22 00:58:09 +0200

    Update db_create.sh

--- script/db_create.sh
+++ script/db_create.sh
@@ -10,23 +10,24 @@ QRYPATH=$HOME/Dropbox/Github/My-Collection/query/
 
 #Creating colour variables
 RED="`tput setaf 1`"
+GRN="`tput setaf 2`"
 YLW="`tput setaf 3`"
 NC="`tput sgr0`"
 
 #Test if database exists, otherwise create the database
 cd $DBPATH
 if [ -f $DBNAME ]; then
- echo "The database '${RED}$DBNAME${NC}' exists."
+ echo "The database '${GRN}$DBNAME${NC}' already exists."
   ls -ltr $DBNAME
 else
  echo "Creating database into: '$DBPATH'..."
   sqlite3 $DBNAME .databases .exit
   chmod 777 $DBNAME
- echo "Creating schema into '${RED}$DBNAME${NC}'..."
+ echo "Creating schema into '${GRN}$DBNAME${NC}'..."
   sqlite3 $DBNAME ".read ${QRYPATH}db_schema.sql"
- echo "Creating triggers into '${RED}$DBNAME${NC}'..."
+ echo "Creating triggers into '${GRN}$DBNAME${NC}'..."
   sqlite3 $DBNAME ".read ${QRYPATH}db_trigger.sql"
- echo "Populating lookup tables into '${RED}$DBNAME${NC}'..."
+ echo "Populating lookup tables into '${GRN}$DBNAME${NC}'..."
   sqlite3 $DBNAME ".read ${QRYPATH}db_lookup.sql"
 fi
 
