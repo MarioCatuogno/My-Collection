99496c8fdc2b0b93bf2f8c2feb0c3645ad043976
Mario Catuogno <catuogno.mario88@gmail.com>
2017-10-22 11:53:13 +0200

    Update db_create.sh

--- script/db_create.sh
+++ script/db_create.sh
@@ -1,33 +1,25 @@
-#!/bin/bash
+#!/bin/bash db_create.sh
 
-#Creating path variables
-DBNAME=my_collection.db
-GITPATH=$HOME/Dropbox/Github/My-Collection/
-DBPATH=$HOME/Dropbox/Github/My-Collection/db/
-SCRPATH=$HOME/Dropbox/Github/My-Collection/script/
-CSVPATH=$HOME/Dropbox/Github/My-Collection/csv/
-QRYPATH=$HOME/Dropbox/Github/My-Collection/query/
-
-#Creating colour variables
-RED="`tput setaf 1`"
-GRN="`tput setaf 2`"
-YLW="`tput setaf 3`"
-NC="`tput sgr0`"
+#Load config script
+source config.sh
 
 #Test if database exists, otherwise create the database
 cd $DBPATH
 if [ -f $DBNAME ]; then
- echo "The database '${GRN}$DBNAME${NC}' already exists."
-  ls -ltr $DBNAME
+    echo "The database '${GRN}$DBNAME${NC}' already exists."
+    ls -ltr $DBNAME
 else
- echo "Creating database into: '$DBPATH'..."
-  sqlite3 $DBNAME .databases .exit
-  chmod 777 $DBNAME
- echo "Creating schema into '${GRN}$DBNAME${NC}'..."
-  sqlite3 $DBNAME ".read ${QRYPATH}db_schema.sql"
- echo "Creating triggers into '${GRN}$DBNAME${NC}'..."
-  sqlite3 $DBNAME ".read ${QRYPATH}db_trigger.sql"
- echo "Populating lookup tables into '${GRN}$DBNAME${NC}'..."
-  sqlite3 $DBNAME ".read ${QRYPATH}db_lookup.sql"
+    echo "Creating database into: '$DBPATH'..."
+    sqlite3 $DBNAME .databases .exit
+    chmod 777 $DBNAME
+#Create tables
+    echo "Creating schema into '${GRN}$DBNAME${NC}'..."
+    sqlite3 $DBNAME ".read ${QRYPATH}db_schema.sql"
+#Create triggers
+    echo "Creating triggers into '${GRN}$DBNAME${NC}'..."
+    sqlite3 $DBNAME ".read ${QRYPATH}db_trigger.sql"
+#Populate LK tables
+    echo "Populating lookup tables into '${GRN}$DBNAME${NC}'..."
+    sqlite3 $DBNAME ".read ${QRYPATH}db_lookup.sql"
 fi
-
+exit
