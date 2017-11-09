f4c48198c93dd1adad20c3356c7ca2bcb1646659
Mario Catuogno <catuogno.mario88@gmail.com>
2017-11-09 21:51:00 +0100

    Update db_insert.sh

--- script/db_insert.sh
+++ script/db_insert.sh
@@ -7,9 +7,10 @@ source config.sh
 cd $DBPATH
 if [ -f $DBNAME ]; then
     echo "The database '${GRN}$DBNAME${NC}' already exists."
-    echo "Importing CSV into: '${GRN}$DBNAME${NC}'..."
 #Insert CSV into DB
+    echo "Importing movie list CSV into: '${GRN}$DBNAME${NC}'..."
     sqlite3 $DBNAME ".mode csv" ".separator ';'" ".import ${CSVPATH}insert_list.csv my_movies_list"
+    echo "Importing movie seen CSV into: '${GRN}$DBNAME${NC}'..."
     sqlite3 $DBNAME ".mode csv" ".separator ';'" ".import ${CSVPATH}insert_seen.csv my_movies_seen"
 else
     echo "The database '${GRN}$DBNAME${NC}' doesn't exists."
